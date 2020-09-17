71dd5a0b79d44d13bf4bc79ed7e2f8d0534df2ea
Dev.HyunSang <hyun.sang@parkhyunsang.com>
2020-09-17 18:25:21 +0900

    [ADD] GET Display Media

---
+++ getdisplaymedia/LitElement.js
@@ -0,0 +1,38 @@
+import { TemplateResult } from 'lit-html';
+import { render } from 'lit-html/lib/shady-render';
+import { UpdatingElement } from './lib/updating-element.js';
+export * from './lib/updating-element.js';
+export * from './lib/decorators.js';
+export { html, svg } from 'lit-html/lit-html';
+export class LitElement extends UpdatingElement {
+    /**
+     * Updates the element. This method reflects property values to attributes
+     * and calls `render` to render DOM via lit-html. Setting properties inside
+     * this method will *not* trigger another update.
+     * * @param _changedProperties Map of changed properties with old values
+     */
+    update(changedProperties) {
+        super.update(changedProperties);
+        const templateResult = this.render();
+        if (templateResult instanceof TemplateResult) {
+            this.constructor
+                .render(templateResult, this.renderRoot, { scopeName: this.localName, eventContext: this });
+        }
+    }
+    /**
+     * Invoked on each update to perform rendering tasks. This method must return
+     * a lit-html TemplateResult. Setting properties inside this method will *not*
+     * trigger the element to update.
+     * @returns {TemplateResult} Must return a lit-html TemplateResult.
+     */
+    render() { }
+}
+/**
+ * Render method used to render the lit-html TemplateResult to the element's
+ * DOM.
+ * @param {TemplateResult} Template to render.
+ * @param {Element|DocumentFragment} Node into which to render.
+ * @param {String} Element name.
+ */
+LitElement.render = render;
+//# sourceMappingURL=lit-element.js.map
